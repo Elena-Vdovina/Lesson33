63af5a1b608a2ace550bba5262739693fbddc457
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-03 14:27:00 +0100

    Add Homework2

---
+++ src/Homework2.java
@@ -0,0 +1,6 @@
+public class Homework2 {
+  public static void main(String[] args) {
+// Совместите программы Task1Months и Task2MonthsEnums, чтобы число дней в месяце читалось
+// из файла res/months.csv, но сохранялось Map<Month, Integer>.
+  }
+}
